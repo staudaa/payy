77e6d5937e08f06c2687f365e6bb643954eb6008
staudaa <radaraissa2006@gmail.com>
2024-11-05 21:53:58 +0700

    wan

--- remedial.py
+++ remedial.py
@@ -1,12 +1,19 @@
 angka = int(input("angka akhir: ")) 
         
-for j in range (1, angka+1) :
-    if j % 3 == 0:
-        if j % 5 == 0:
-            print("FizzBuzz")
+if 1 <= angka <= 999 :
+    for j in range (angka+1) :
+        if j % 3 == 0:
+            if j % 5 == 0:
+                print("FizzBuzz")
+            else:
+                print("Fizz")
+        elif j % 5 == 0:
+            print("Buzz")
         else:
-            print("Fizz")
-    elif j % 5 == 0:
-        print("Buzz")
-    else:
-        print(j)
+            print(j)
+        
+else :
+    print("angka diluar kondisi")
+
+        
+        
