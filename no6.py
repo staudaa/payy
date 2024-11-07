ac054f522b8cbf03d3e61204dffb04974315e045
Faza <90262159+FaruuZa@users.noreply.github.com>
2024-11-07 17:26:37 +0700

    no 6

---
+++ no6.py
@@ -0,0 +1,31 @@
+kiri = "q, w, e, r, t, a, s, d, f, g, z, x, c, v, b".split(", ")
+kanan = "y, u, i, o, p, h, j, k, l, n, m".split(", ")
+
+masukan = input()
+tangan = [] # 0 = kiri, 1 = kanan
+for i in masukan:
+    for j in kiri:
+        if i == j:
+            tangan.append(0)
+            break
+    for j in kanan:
+        if i == j:
+            tangan.append(1)
+            break
+posisi = ""
+aaa = True
+print(tangan)
+for i in tangan:
+    if i == 0:
+        if posisi == "kiri":
+            aaa = False
+            break
+        else:
+            posisi = "kiri"
+    else:
+        if posisi == "kanan":
+            aaa = False
+            break
+        else:
+            posisi = "kanan"
+print(aaa)
